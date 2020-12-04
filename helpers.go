ec91a4202e66d14e291f17010f4c2f7f59f95c1f
Willem de Groot <gwillem@gmail.com>
2020-12-04 16:04:47 +0100

    Can now update database in --add mode

---
+++ helpers.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+func isCmsRoot(root string) bool {
+	for _, testPath := range cmsPaths {
+		full := root + testPath
+		if pathExists(full) {
+			return true
+		}
+	}
+	return false
+}
+
+func shouldHighlight(b []byte) bool {
+	for _, p := range highlightPatterns {
+		m, _ := regexp.Match(p, b)
+		if m {
+			return true
+		}
+	}
+	return false
+}
+
+func normalizeLine(b []byte) []byte {
+	// Also strip slashes comments etc
+	b = bytes.TrimSpace(b)
+	for _, prefix := range skipLines {
+		if bytes.HasPrefix(b, prefix) {
+			return []byte{}
+		}
+	}
+	return b
+}
+
+func pathExists(p string) bool {
+	_, err := os.Stat(p)
+	return err == nil
+}
+
+func isDir(path string) bool {
+	fi, err := os.Stat(path)
+	return err == nil && fi.IsDir()
+}
+
+func hasValidExt(path string) bool {
+	got := strings.TrimLeft(filepath.Ext(path), ".")
+	for _, want := range scanExts {
+		if got == want {
+			return true
+		}
+	}
+	return false
+}
+
+func logVerbose(a ...interface{}) {
+	if logLevel >= 3 {
+		fmt.Println(a...)
+	}
+}
+func logInfo(a ...interface{}) {
+	fmt.Println(a...)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func hash(b []byte) [16]byte {
+	return md5.Sum(b)
+}
+
+func pathHash(p string) [16]byte {
+	return hash([]byte("path:" + p))
+}
---
+++ init.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/fatih/color"
+	"github.com/gwillem/urlfilecache"
+	"github.com/jessevdk/go-flags"
+)
+
+
+type (
+	hashDB map[[16]byte]bool
+
+	walkStats struct {
+		totalFiles          int
+		filesWithChanges    int
+		filesWithoutChanges int
+	}
+
+	Args struct {
+		Path struct {
+			Path string `positional-arg-name:"<path>"`
+		} `positional-args:"yes" description:"Scan file or dir" required:"true"`
+		Database string `short:"d" long:"database" description:"Hash database path (default: download Sansec database)"`
+		Add      bool   `short:"a" long:"add" description:"Add new hashes to DB, do not check"`
+		Full     bool   `short:"f" long:"full" description:"Scan everything, not just core paths."`
+		Verbose  bool   `short:"v" long:"verbose" description:"Show what is going on"`
+	}
+)
+
+const (
+	hashDBURL = "https://sansec.io/ext/files/corediff.bin"
+)
+
+var (
+	boldred   = color.New(color.FgHiRed, color.Bold).SprintFunc()
+	grey      = color.New(color.FgHiBlack).SprintFunc()
+	boldwhite = color.New(color.FgHiWhite).SprintFunc()
+	alarm     = color.New(color.FgHiWhite, color.BgHiRed, color.Bold).SprintFunc()
+	green     = color.New(color.FgGreen).SprintFunc()
+
+	logLevel = 1
+
+	globalDB hashDB
+
+	scanExts  = []string{"php", "phtml", "js", "htaccess", "sh"}
+	skipLines = [][]byte{
+		[]byte("*"),
+		[]byte("/*"),
+		[]byte("//"),
+		[]byte("#"),
+	}
+
+	cmsPaths = []string{
+		"/app/etc/local.xml",
+		"/app/etc/env.php",
+		"/wp-config.php",
+	}
+
+	highlightPatterns = []string{
+		`\$_[A-Z]`,
+		`["']\s*\.\s*['"]`,
+		`die\(`,
+		`base64_`,
+		`@(unlink|include|mysql)`,
+		`../../..`,
+		`hex2bin`,
+		`fopen`,
+		`file_put_contents`,
+		`file_get_contents`,
+	}
+)
+
+func setup() *Args {
+
+	var err error
+	color.NoColor = false
+
+	args := &Args{}
+	argParser := flags.NewParser(args, flags.HelpFlag|flags.PrintErrors|flags.PassDoubleDash)
+	if _, err := argParser.Parse(); err != nil {
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrRequired {
+		} else {
+			// log.Fatal(err)
+			// fmt.Println("Config parse error:", err)
+		}
+		os.Exit(1)
+	}
+
+	if args.Verbose {
+		logLevel = 3
+	}
+
+	if args.Database == "" {
+		args.Database = urlfilecache.ToPath(hashDBURL)
+	}
+
+	// postprocessing
+	args.Path.Path, err = filepath.Abs(args.Path.Path)
+	check(err)
+	args.Path.Path, err = filepath.EvalSymlinks(args.Path.Path)
+	check(err)
+
+	if !pathExists(args.Path.Path) {
+		fmt.Println("Path", args.Path.Path, "does not exist?")
+		os.Exit(1)
+	}
+
+	// Enforce scanning is target is just a single file
+	if !isDir(args.Path.Path) {
+		args.Full = true
+	}
+
+	// Basic check for application root?
+	if !args.Full && !isCmsRoot(args.Path.Path) {
+		fmt.Println("Path does not seem to be an application root path, so we cannot check official root paths.")
+		fmt.Println("Try again with proper root path, or do a full scan with --full")
+		os.Exit(1)
+	}
+
+	return args
+}
