7fbf53d7a8098e765d89c3db59dfea22578528f8
Willem de Groot <gwillem@gmail.com>
2023-04-02 19:43:42 +0200

    Remove specific test

--- needle_test.go
+++ needle_test.go
@@ -8,7 +8,6 @@ import (
 func Test_Needle(t *testing.T) {
 
 	needles := []string{
-		"path:pub/static/frontend/BodyAndBeach/store/fr_CA/Altima_Lookbookslider/js/jquery.cycle2_1.js",
 		"path:app/code/Magedelight/GeoIp/Controller/Adminhtml/Currencymapping/Delete.php",
 	}
 	dbpath := "m2.db"
